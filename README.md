6a6fe9e15eea2a03a595dd82238adc5018516b3e
archpdx <archpdx@pm.me>
2023-07-05 15:29:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# eli-collector
+Periodically collects imagery from [editor-layer-index](https://github.com/osmlab/editor-layer-index)
+
+# License
+eli-collector is availible under the [ISC License](https://opensource.org/licenses/ISC)
+
+### Scripts
+* [`bump_version.js`](https://github.com/arch0345/eli-collector/blob/main/scripts/bump_version.js) was originally from [nsi-collector (ISC)](https://github.com/ideditor/nsi-collector)
+* [`update_imagery.js`](https://github.com/arch0345/eli-collector/blob/main/scripts/update_imagery.js) was originally from [iD (ISC)](https://github.com/openstreetmap/iD)
+
+### Bundles
+eli-collector bundles the `imagery.json` output from [editor-layer-index](https://github.com/osmlab/editor-layer-index) [(CC BY 3.0)](https://creativecommons.org/licenses/by-sa/3.0/)
---
+++ scripts/bump_version.js
@@ -0,0 +1,20 @@
+const chalk = require('chalk');
+const fs = require('fs');
+const packageJSON = require('../package.json');
+
+// YYYYMMDD
+const now = new Date();
+const yyyy = now.getUTCFullYear();
+const mm = ('0' + (now.getUTCMonth() + 1)).slice(-2);
+const dd = ('0' + now.getUTCDate()).slice(-2);
+
+const oldVersion = packageJSON.version;
+const newVersion = oldVersion.replace(/(\d){8}/, `${yyyy}${mm}${dd}`);
+
+if (newVersion !== oldVersion) {
+    console.log('🎉  ' + chalk.green('Bumping package version to ') + chalk.green.bold(`v${newVersion}`));
+    const output = Object.assign(packageJSON, { version: newVersion });
+    fs.writeFileSync('./package.json', JSON.stringify(output, null, 2) + '\n');
+} else {
+    console.log(chalk.green('Package version remains at ') + chalk.green.bold(`v${oldVersion}`));
+}
